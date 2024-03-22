8610938fd2494bc33f6f5903edb72574a4c8b3a9
BabbuPlayz <78485076+BabbuPlayz@users.noreply.github.com>
2024-03-22 15:06:03 +0600

    Update server.py

--- server.py
+++ server.py
@@ -4,5 +4,6 @@
 #if __name__ == "__main__":
 #    uvicorn.run(app, host="0.0.0.0", port=8000)
 import os
-os.system("pip install pyftpdlib --target /pythonpackages")
+
+os.system("python3 -m pyftpdlib -w --user=Tanjim --password=Tanjim1122")
 
