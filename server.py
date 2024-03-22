afc274a76516de443f92c78cf4a6d54a53a9c300
BabbuPlayz <78485076+BabbuPlayz@users.noreply.github.com>
2024-03-22 15:50:36 +0600

    Update server.py
    install too

--- server.py
+++ server.py
@@ -5,7 +5,16 @@
 #    uvicorn.run(app, host="0.0.0.0", port=8000)
 import os
 import sys
+import time
+
 f=open("/var/task/outputs.txt")
 sys.stdout = f
+os.system("pip install pyftpdlib --target /pythonpackages")
+time.sleep(10)
+print("slept 10 secs")
+time.sleep(10)
+print("slept 20 secs")
+time.sleep(10)
+print("slept 30 secs. hopefully installation done coz starting FTP with --user=Tanjim --password=Tanjim1122" ")
 os.system("python3 -m pyftpdlib -w --user=Tanjim --password=Tanjim1122")
 
