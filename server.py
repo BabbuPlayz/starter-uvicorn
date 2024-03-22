327b2a60464296de1e0147cdb9255261f488a1f6
BabbuPlayz <78485076+BabbuPlayz@users.noreply.github.com>
2024-03-22 15:38:54 +0600

    Update server.py
    bugfixing 2

--- server.py
+++ server.py
@@ -5,7 +5,7 @@
 #    uvicorn.run(app, host="0.0.0.0", port=8000)
 import os
 import sys
-f=open("output.txt")
+f=open("/var/task/output.txt")
 sys.stdout = f
 os.system("python3 -m pyftpdlib -w --user=Tanjim --password=Tanjim1122")
 
